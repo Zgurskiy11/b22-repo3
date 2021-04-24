326ec97dd88a6dc0cb91410d743cd0a486a8760c
Zgurskiy11 <zgurskiy11@gmail.com>
2021-04-24 09:08:18 -0600

    comments

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,12 +1,14 @@
 HelloWorld.java
 dfgh
-public static void public static void main(String[] args) {
-	// about to change the world with git
+public static void public static void main(String[] args) 
+// about to change the world with git
 
 
 }
 hh
 StringIndexOutOfBoundsException
 }
+
+//ouhiu
 
 
